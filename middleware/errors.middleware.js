169534a3d7e646a34a033bc87c75b7b6d62c3459
Yaroslav Voloshchuk <bamiks@gmail.com>
2021-04-20 15:00:39 +0300

    update eerors.js

--- middleware/errors.middleware.js
+++ middleware/errors.middleware.js
@@ -2,7 +2,7 @@ module.exports = function (req, res) {
     res.status(404).send(`
         <div>
             <h1>digichlist API</h1>
-            <h1>Not found API path</h1>
+            <h1>Not found API folder</h1>
             <a href='/api-docs'>View API Documentation</a>
         </div>
     `);
